a54ec5bd5a7c74e3eb346bf055812fa307cdae72
Grimhamr <rossiadisj@gmail.com>
2021-08-22 18:28:58 +0300

    add event_attendance controller & #new page w/ params

--- app/controllers/event_attendances_controller.rb
+++ app/controllers/event_attendances_controller.rb
@@ -1,2 +1,30 @@
 class EventAttendancesController < ApplicationController
+
+  def index
+    @event_attendances = EventAttendance.all
+  end
+
+  def new
+    @event_attendance = EventAttendance.new
+    @event = Event.find(params[:event_id]) 
+
+  end
+
+  def create
+    @event_attendance = EventAttendance.new(event_attendance_params)
+
+    respond_to do |format|
+      if @event_attendance.save
+        format.html { redirect_to @event_attendance, notice: "event_attendance was successfully created." }
+        format.json { render :show, status: :created, location: @event_attendance }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event_attendance.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
 end
