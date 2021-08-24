c0a7ae801a2d2620977ee79263ffebfd10d4b24e
Grimhamr <rossiadisj@gmail.com>
2021-08-24 09:52:04 +0300

    forbid users from attending same event twice

--- app/models/event_attendance.rb
+++ app/models/event_attendance.rb
@@ -1,5 +1,7 @@
 class EventAttendance < ApplicationRecord
 
+  validates :user, uniqueness: { scope: :event , message: " already attending. You can't attend the same event twice!" }
+    
 
   
   belongs_to :user
